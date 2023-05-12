383f3dcc583ac77a774ac1d1f834b7dd1d69b6c1
allen <allen.tuazon@gmail.com>
2023-05-12 13:56:00 +1000

    route for get and post api/notes

--- server.js
+++ server.js
@@ -11,16 +11,30 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-// route display notes.html
+// route return the index.html
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// route return the notes.html
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // route to GET notes from db.json
-app.get('api/notes', (req, res) => {
-    const data = fs.readFileSync(path.join('./db/', 'db.json'), 'utf8');
+app.get('/api/notes', (req, res) => {
+    const data = fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8');
     const notes = JSON.parse(data);
     res.json(notes);
+});
+
+// route to post notes to db.json
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    const notesData = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    notesData.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
+    res.json(newNote);
 });
 
 
