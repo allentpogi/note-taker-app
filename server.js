4ebc64c4d74e4182c254461c7cd171357d0b5cae
allen <allen.tuazon@gmail.com>
2023-05-12 16:08:04 +1000

    added delete function

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
 
@@ -23,20 +24,37 @@ app.get('/notes', (req, res) =>
 
 // route to GET notes from db.json
 app.get('/api/notes', (req, res) => {
-    const data = fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8');
-    const notes = JSON.parse(data);
-    res.json(notes);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const savedNotes = JSON.parse(data);
+          res.json(savedNotes);
+        }
+    });
+
 });
 
 // route to post notes to db.json
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
-    const notesData = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
-    notesData.push(newNote);
-    fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
+    newNote.id = uuid();
+    const notesTosave = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    
+    notesTosave.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notesTosave));
     res.json(newNote);
 });
 
+//route to delete notes from db.json
+app.delete('/api/notes/:id', (req, res) => {
+    const notesData = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    const noteIndex = notesData.findIndex((note) => note.id === req.params.id);
+    notesData.splice(noteIndex, 1);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
+    res.json(notesData);
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
